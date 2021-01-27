9505e419beb61611583d65c3a70a2d9bda3d352e
Anotso <40184359+Anotso@users.noreply.github.com>
2021-01-27 00:22:12 -0300

    Primeiro commit - envio da estrutura básica e telas

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+var ExcelToJSON = function() {
+
+    this.parseExcel = function(file) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {
+                type: 'binary'
+            });
+            workbook.SheetNames.forEach(function(sheetName) {
+                // Here is your object
+                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                console.log(XL_row_object);
+                var json_object = JSON.stringify(XL_row_object);
+                console.log(JSON.parse(json_object));
+                jQuery( '#xlx_json' ).val( json_object );
+            })
+        };
+
+        reader.onerror = function(ex) {
+        console.log(ex);
+        };
+
+        reader.readAsBinaryString(file);
+    };
+};
+
+function handleFileSelect(evt) {
+  
+    var files = evt.target.files; // FileList object
+    var xl2json = new ExcelToJSON();    //Instancia a função para fazer o tratamento documento
+    xl2json.parseExcel(files[0]);
+}
